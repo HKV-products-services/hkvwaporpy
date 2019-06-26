48450388e9f235b39d95bcd1bedc49b9c54d484b
Hoek <hoek@hkv.nl>
2019-06-26 15:26:44 +0200

    latest changes

--- hkvwaporpy/__init__.py
+++ hkvwaporpy/__init__.py
@@ -1,7 +1,7 @@
 from hkvwaporpy.io.fao_wapor_api import __fao_wapor_class
 
 __doc__ = """package for FAO WAPOR API"""
-__version__ = '0.2'
+__version__ = '0.6'
 
 # initiate class for .his-files
 read_wapor = __fao_wapor_class()
