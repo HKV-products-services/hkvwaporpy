cbf1a3aa837dc44827ed8c2c359283f5a15d0504
NB Tran <b.tran@un-ihe.org>
2019-07-19 16:23:00 +0200

    origin hkv

---
+++ hkvwaporpy/__init__.py
@@ -0,0 +1,7 @@
+from hkvwaporpy.fao_wapor_api import __fao_wapor_class
+
+__doc__ = """package for FAO WAPOR API"""
+__version__ = "0.7"
+
+# initiate class for .his-files
+read_wapor = __fao_wapor_class()
